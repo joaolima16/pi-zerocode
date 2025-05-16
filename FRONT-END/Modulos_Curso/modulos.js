4718b239facb1622788bd053515f41f76f96c12e
Rich-Miranda <mirandarichard345@gmail.com>
2025-05-15 21:37:04 -0300

    Ajustado as telas

--- FRONT-END/Modulos_Curso/modulos.js
+++ FRONT-END/Modulos_Curso/modulos.js
@@ -1,30 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    renderClassrooms();
-
+  renderClassrooms();
 });
 
 const getClassroom = async (id) => {
-    const courseId = localStorage.getItem("courseId");
-    let dataClassrooms = [];
-    const url = `http://localhost:8080/courses/${courseId}`
-    await fetch(url)
+  const courseId = localStorage.getItem("courseId");
+  let dataClassrooms = [];
+  const url = `http://localhost:8080/courses/${courseId}`;
+  await fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        document.querySelector(".title-course").innerHTML = `<h2>${data.name}</h2>`;
-        dataClassrooms = data.classrooms;
+      document.querySelector(
+        ".title-course"
+      ).innerHTML = `<h2>${data.name}</h2>`;
+      dataClassrooms = data.classrooms;
     })
     .catch((err) => console.error(err));
-    return dataClassrooms;
+  return dataClassrooms;
 };
-const renderClassrooms = async() =>{
-    const dataClassrooms = await getClassroom();
-    const classroomsContainer = document.querySelector(".accordion");
-    classroomsContainer.innerHTML = "";
-    dataClassrooms.map((classroom) =>{
-        console.log(classroom)
-        return(
-            classroomsContainer.innerHTML += `
-                <div class="accordion-item">
+const renderClassrooms = async () => {
+  const dataClassrooms = await getClassroom();
+  const classroomsContainer = document.querySelector(".accordion");
+  classroomsContainer.innerHTML = "";
+  dataClassrooms.map((classroom) => {
+    console.log(classroom);
+    return (classroomsContainer.innerHTML += `
+                <div class="accordion-item" key="${classroom.id}">
                     <h2 class="accordion-header">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
@@ -33,23 +33,23 @@ const renderClassrooms = async() =>{
                     </h2>
                     <div id="flush-collapseOne" class="accordion-collapse collapse"
                         data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Duração da aula: ${formatDuration(classroom.duration)}</div>
+                        <div class="accordion-body">Duração da aula: ${formatDuration(
+                          classroom.duration
+                        )}</div>
                     </div>
                 </div>
-            `
-        )
-    })
-}
+            `);
+  });
+};
 function formatDuration(minutos) {
-    const horas = Math.floor(minutos / 60);
-    const mins = minutos % 60;
-  
-    if (horas > 0 && mins > 0) {
-      return `${horas}h ${mins}min`;
-    } else if (horas > 0) {
-      return `${horas}h`;
-    } else {
-      return `${mins} min`;
-    }
+  const horas = Math.floor(minutos / 60);
+  const mins = minutos % 60;
+
+  if (horas > 0 && mins > 0) {
+    return `${horas}h ${mins}min`;
+  } else if (horas > 0) {
+    return `${horas}h`;
+  } else {
+    return `${mins} min`;
   }
-  
+}
